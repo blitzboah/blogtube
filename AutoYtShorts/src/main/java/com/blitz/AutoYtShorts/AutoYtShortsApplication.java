ff20980015a338620228f2b3302d74872d10e7f0
blitzboah <manodrabhumin@gmail.com>
2024-09-09 17:05:37 +0530

    scraping and summarization done

---
+++ AutoYtShorts/src/main/java/com/blitz/AutoYtShorts/AutoYtShortsApplication.java
@@ -0,0 +1,50 @@
+package com.blitz.AutoYtShorts;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class AutoYtShortsApplication {
+
+	public static void main(String[] args) {
+
+		String filePath = "./links.txt";
+
+		try{
+
+			Path path = Paths.get(filePath);
+			List<String> blogLinks = Files.readAllLines(path);
+			String url = blogLinks.getFirst();
+
+			Document doc = Jsoup.connect(url).get();
+
+			String title = doc.title();
+			Elements paragraphs= doc.select("p");
+
+			StringBuilder blogContent = new StringBuilder();
+			blogContent.append("title: ").append(title).append("\n");
+			for (Element paragraph : paragraphs){
+				blogContent.append(paragraph.text());
+			}
+
+			try(FileWriter writer = new FileWriter("./content.txt")){
+				writer.write(blogContent.toString());
+			}
+			//blogLinks.removeFirst();
+			//Files.write(path, blogLinks);
+
+		}
+		catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
---
+++ AutoYtShorts/src/test/java/com/blitz/AutoYtShorts/AutoYtShortsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blitz.AutoYtShorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoYtShortsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
