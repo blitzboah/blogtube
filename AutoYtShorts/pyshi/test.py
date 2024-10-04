fb4211353a8e540c1812cee080bba3414774b0be
blitz <146631108+blitzboah@users.noreply.github.com>
2024-10-04 20:45:10 +0530

    Delete AutoYtShorts directory

--- AutoYtShorts/pyshi/test.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nltk.download('punkt_tab')
-from nltk.tokenize import sent_tokenize
-
-text = "This is a sentence. Here is another sentence."
-sentences = sent_tokenize(text)
-print(sentences)
-
